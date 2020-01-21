e8952a94f1f5d487d00b5e25f90b99cf28ed4057
Or <or15yakov@gmail.com>
2020-01-21 17:26:35 +0200

    comments on TableEntry

--- MusicWord/Models/TableEntry.cs
+++ MusicWord/Models/TableEntry.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 using Caliburn.Micro;
 namespace MusicWord.Models
 {
+    /// <summary>
+    /// <c>TableEntry</c>
+    /// a class that represents an entry of the score table
+    /// </summary>
     class TableEntry : PropertyChangedBase
     {
-
+        // the actual column values of the entry, which are the following properties
         public string PlayerName { get; set; }
         public int PlayerScore { get; set; }
         public string Category { get; set; }
