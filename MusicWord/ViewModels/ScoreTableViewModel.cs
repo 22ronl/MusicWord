a429630ad4704394f458e4392b188c0736e39ad7
Or <or15yakov@gmail.com>
2020-01-21 17:50:03 +0200

    comments on ScoreTableViewModel

--- MusicWord/ViewModels/ScoreTableViewModel.cs
+++ MusicWord/ViewModels/ScoreTableViewModel.cs
@@ -9,15 +9,25 @@ using System.Windows;
 
 namespace MusicWord.ViewModels
 {
-    
+    /// <summary>
+    /// <c>ScoreTableViewModel</c>
+    /// the score table viewModel
+    /// </summary>
     class ScoreTableViewModel : AppScreen
     {
+        /// <summary>
+        /// a collection in which every item can be binded
+        /// </summary>
         public BindableCollection<TableEntry> ScoreTable { get; set; }
 
+        // init score table collection from the score table list in the SQLServerModel
         public ScoreTableViewModel()
         {
             ScoreTable = new BindableCollection<TableEntry>(SQLServerModel.getScoreTable());
         }
+        /// <summary>
+        /// function that makes the game move to the next window
+        /// </summary>
         public void Next()
         {
             NextScreen();
